b5ba3562d11253e84b781deb56bcd575161ad6e8
thiagoweverson <124640710+thiagoweverson@users.noreply.github.com>
2023-05-12 00:24:27 -0300

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById('my-form').addEventListener('submit', (e) =>{
-      e.preventDefault()
-      fetch(e.target.action, {
-            method: 'POST',
-            body: new FormData(e.target)
-      }).then(() => {
-            window.location.href = './fim.html'
-      })
-})
